2eb3ff0d4f74d51e08060d8ef5d680f0469b8b92
KristinaCapistrano <32004340+kristinacapistrano@users.noreply.github.com>
2020-04-04 17:01:03 -0700

    working serialization of json - cpp
    assign 4

---
+++ Track.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+
+//Created file so far that has music.json as name of txt file
+//next objective is to read info from the url given  - this has been started
+//line above is done
+//next objective is to get only nessecary data
+using namespace std;
+
+class Track {
+
+
+public:
+
+  
+  /*serialize and deserialize JSON*/
+  void jsonreadwrite(){
+ 
+    //-- serialize --//
+    ifstream ifs("FMdata.json");
+    Json::Reader reader;
+    Json::Value obj ;
+    reader.parse(ifs, obj);
+
+
+    string album = obj["album"]["name"].asString();
+    string artist = obj["album"]["artist"].asString();
+    cout<<"Album: " << album  << endl;
+    cout << "Artist: " << artist << endl << endl;
+    const Json::Value& track = obj["album"]["tracks"]["track"];
+
+    for (int i = 0; i < track.size(); i++){
+      string time  = track[i]["duration"].asString();
+      int nduration = std::stoi(time);
+      int minute  = nduration / 60;
+      int sec = nduration - (minute*60);
+      string ssec = to_string(sec);
+      if (sec == 6) { minute + 1; sec = 0;}
+      if (sec<10){ ssec = "0"+ssec; }
+      //cout << "time - " << minute << ":" << sec << endl;
+      time = (std::to_string(minute))+":"+ssec;
+      string title = track[i]["name"].asString();
+      string rank = track[i]["@attr"]["rank"].asString();
+      cout << "title: " << title<< " and duration: " << time
+	   <<" and rank: " << rank  << endl;
+
+      
+    }
+  }
+
+};
+  
