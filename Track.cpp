190031ce92941605a3a36b5c8af0755bea458ead
KristinaCapistrano <32004340+kristinacapistrano@users.noreply.github.com>
2020-04-05 23:15:25 -0700

    search implemented
    function working but in order for search to refresh with the new searched info, user has to exit program and run again. search updates fmdata.json file and media.json file with new searched info.

--- Track.cpp
+++ Track.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
+#include "Track.hpp"
 
-//Created file so far that has music.json as name of txt file
-//next objective is to read info from the url given  - this has been started
-//line above is done
-//next objective is to get only nessecary data
+/**
+ *@author Kristina Capistrano 
+ *@version 4.4.20
+ *Ser321 Principles of Distributed Software Systems 
+ */
+
 using namespace std;
-
-class Track {
-
-
-public:
-
   
-  /*serialize and deserialize JSON*/
-  void jsonreadwrite(){
+  /*serialize and deserialize JSON - constructor will initiate a json fil
+   *constructor to initialize an object from a Json::Value
+   */
+void Track::jsonreadwrite(){
  
     ifstream ifs("FMdata.json");
     Json::Reader reader;//read from fm json file 
@@ -23,15 +22,15 @@ public:
     reader.parse(ifs, obj);
 
     ofstream _file;
-    _file.open("music.json"); //create a json file to store only needed data
+    _file.open("media.json"); //open a json file to store only needed data
     Json::Value music;
 
     string album = obj["album"]["name"].asString();
     string artist = obj["album"]["artist"].asString();
     cout<<"Album: " << album  << endl;
     cout << "Artist: " << artist << endl << endl;
+    
     const Json::Value& track = obj["album"]["tracks"]["track"];
-
     for (int i = 0; i < track.size(); i++){
       string time  = track[i]["duration"].asString();
       int nduration = std::stoi(time);
@@ -49,15 +48,35 @@ public:
       music[title]["title"]=title;
       music[title]["author"]=artist;
       music[title]["album"]=album;
+      music[title]["rank"]=rank;
+      music[title]["time"]=time;
+      music[title]["genre"]="pop";
+      music[title]["typeExt"]="mp3";
+      music[title]["summary"] = obj["album"]["wiki"]["summary"].asString();
 
-
-
+     
     }//forloop
      Json::StyledWriter _writer;
-     _file << _writer.write(music);
-    _file.close();
+     _file << _writer.write(music); //write to music.json file 
+     _file.close(); //close file 
 
-  }//method 
+  };//method jsonreadwrite
 
-};//class 
+
+/**
+ * Get image to use for the music player - will xlarge copy
+ */
+const char* Track::getImage(){
+    ifstream ifs("FMdata.json");
+    Json::Reader reader;//read from fm json file 
+    Json::Value obj ;
+    reader.parse(ifs, obj);
+    
+    const Json::Value& _img = obj["album"]["image"];
+    std::string  str = _img[3]["#text"].asString();
+    const char* img = str.c_str();
+    cout <<"loading image const char* .... " <<  img << endl;
+    return img;
+  }
+ 
   
