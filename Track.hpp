c87d3a6c081bfbd72c10d6af936420eecb565375
KristinaCapistrano <32004340+kristinacapistrano@users.noreply.github.com>
2020-04-05 23:15:41 -0700

    Add files via upload

---
+++ Track.hpp
@@ -0,0 +1,19 @@
+   #pragma once
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+using namespace std;
+
+/**
+ *@author Kristina Capistrano 
+ *@version 4.4.20
+ *Ser321 Principles of Distributed Software Systems 
+ */
+
+class Track{
+
+public :
+  void jsonreadwrite();
+  const char*  getImage();
+};
+  
